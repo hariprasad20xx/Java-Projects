1475792e11ba4956a133acba3daaa0fe597585d7
Hariprasad <hariprasadjnv963@gmail.com>
2025-10-09 10:55:46 +0530

    Add sudoku board design

--- SudokuGameApp/src/sudoku/userinterface/UserInterfaceImpl.java
+++ SudokuGameApp/src/sudoku/userinterface/UserInterfaceImpl.java
@@ -1,15 +1,36 @@
 package sudoku.userinterface;
 
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import sudoku.problemdomain.Coordinates;
 import sudoku.problemdomain.SudokuGame;
+
+import java.util.HashMap;
 
 public class UserInterfaceImpl implements IUserInterfaceContract.View,
         EventHandler<KeyEvent > {
-    @Override
-    public void handle(KeyEvent keyEvent) {
 
-    }
+    private final Stage stage;
+    private final Group root;
+
+    // keeping track of 81 different text fields
+    private HashMap<Coordinates, SudokuTextField> textFieldCoordinates;
+
+    private IUserInterfaceContract.EventListener listener;
+
+    private static final double WINDOW_Y = 732;
+    private static final double WINDOW_X = 668;
+    private static final double BOARD_PADDING = 50;
+    private static final double BOARD_X_AND_Y = 576;
+
+    private static final Color WINDOW_BACKGROUND_COLOR = Color.rgb(0, 150, 136);
+    private static final Color BOARD_BACKGROUND_COLOR = Color.rgb(224, 242, 241);
+    private static final String SUDOKU = "Sudoku";
+
+
 
     @Override
     public void setListener(IUserInterfaceContract.EventListener listener) {
@@ -33,6 +54,11 @@ public class UserInterfaceImpl implements IUserInterfaceContract.View,
 
     @Override
     public void showError(String message) {
+
+    }
+
+    @Override
+    public void handle(KeyEvent keyEvent) {
 
     }
 }
