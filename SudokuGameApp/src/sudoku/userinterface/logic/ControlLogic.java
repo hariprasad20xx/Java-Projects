5e2f3a1dee2f840416a6f75d04b74eace53843c8
Hariprasad <hariprasadjnv963@gmail.com>
2025-10-10 21:01:00 +0530

    Add constructor

--- SudokuGameApp/src/sudoku/userinterface/logic/ControlLogic.java
+++ SudokuGameApp/src/sudoku/userinterface/logic/ControlLogic.java
@@ -1,8 +1,19 @@
 package sudoku.userinterface.logic;
 
+import sudoku.problemdomain.IStorage;
 import sudoku.userinterface.IUserInterfaceContract;
 
 public class ControlLogic implements IUserInterfaceContract.EventListener {
+
+    private IStorage storage;
+
+    private IUserInterfaceContract.View view;
+
+    public ControlLogic(IStorage storage, IUserInterfaceContract.View view) {
+        this.storage = storage;
+        this.view = view;
+    }
+
     @Override
     public void onSudokuInput(int x, int y, int input) {
 
