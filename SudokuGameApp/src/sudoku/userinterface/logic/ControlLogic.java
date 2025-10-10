131a60764e4414942921db7e33df44f032172474
Hariprasad <hariprasadjnv963@gmail.com>
2025-10-10 21:13:56 +0530

    Implement onSudokuInput method

--- SudokuGameApp/src/sudoku/userinterface/logic/ControlLogic.java
+++ SudokuGameApp/src/sudoku/userinterface/logic/ControlLogic.java
@@ -1,7 +1,12 @@
 package sudoku.userinterface.logic;
 
+import sudoku.constants.GameState;
+import sudoku.constants.Messages;
 import sudoku.problemdomain.IStorage;
+import sudoku.problemdomain.SudokuGame;
 import sudoku.userinterface.IUserInterfaceContract;
+
+import java.io.IOException;
 
 public class ControlLogic implements IUserInterfaceContract.EventListener {
 
@@ -16,7 +21,27 @@ public class ControlLogic implements IUserInterfaceContract.EventListener {
 
     @Override
     public void onSudokuInput(int x, int y, int input) {
+        try {
+            SudokuGame gameData = storage.getGameData();
+            int[][] newGridState = gameData.getCopyOfGridState();
+            newGridState[x][y] = input;
 
+            gameData = new SudokuGame(
+                    GameLogic.checkForCompletion(newGridState),
+                    newGridState
+            );
+
+            storage.updateGameData(gameData);
+
+            view.updateSquare(x, y, input);
+
+            if (gameData.getGameState() == GameState.COMPLETE) {
+                view.showDialog(Messages.GAME_COMPLETE);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            view.showError(Messages.ERROR);
+        }
     }
 
     @Override
