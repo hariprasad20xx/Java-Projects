c05b02bc54497232dc586a6501ae3b1706648873
Hariprasad <hariprasadjnv963@gmail.com>
2025-10-11 21:48:06 +0530

    Create getSolvedGame method

--- SudokuGameApp/src/sudoku/computationlogic/GameGenerator.java
+++ SudokuGameApp/src/sudoku/computationlogic/GameGenerator.java
@@ -1,5 +1,50 @@
 package sudoku.computationlogic;
 
-public class GameGenerator {
+import sudoku.problemdomain.Coordinates;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static sudoku.problemdomain.SudokuGame.GRID_BOUNDARY;
+
+public class GameGenerator {
+    public static int[][] getNewGameGrid() {
+        return unsolveGame(getSolvedGame());
+    }
+
+    private static int[][] getSolvedGame() {
+        Random random = new Random(System.currentTimeMillis());
+        int[][] newGrid = new int[GRID_BOUNDARY][GRID_BOUNDARY];
+
+        for (int value = 1; value <= GRID_BOUNDARY; value++) {
+            int allocations = 0;
+            int interrupt = 0;
+
+            List<Coordinates> allocTracker = new ArrayList<>();
+
+            int attempts = 0;
+
+            while (allocations < GRID_BOUNDARY) {
+                if (interrupt > 200) {
+                    allocTracker.forEach(coord -> {
+                        newGrid[coord.getX()][coord.getY()] = 0
+                    });
+
+                    interrupt = 0;
+                    allocations = 0;
+                    allocTracker.clear();
+                    attempts++;
+
+                    if (attempts > 500) {
+                        clearArray(newGrid);
+                        attempts = 0;
+                        value = 1;
+                    }
+                }
+            }
+        }
+
+        return new int[0][];
+    }
 }
