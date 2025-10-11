aadcdee0ca2ff71e97ac9c7ced1f29cbce13e977
Hariprasad <hariprasadjnv963@gmail.com>
2025-10-11 21:12:08 +0530

    Add method to copy sudoku array values

--- SudokuGameApp/src/sudoku/computationlogic/SudokuUtilities.java
+++ SudokuGameApp/src/sudoku/computationlogic/SudokuUtilities.java
@@ -1,4 +1,13 @@
 package sudoku.computationlogic;
 
+import sudoku.problemdomain.SudokuGame;
+
 public class SudokuUtilities {
+    public static void copySudokuArrayValues(int[][] oldArray, int[][] newArray) {
+        for (int xIndex = 0; xIndex < SudokuGame.GRID_BOUNDARY; xIndex++) {
+            for (int yIndex = 0; yIndex < SudokuGame.GRID_BOUNDARY; yIndex++) {
+                newArray[xIndex][yIndex] = oldArray[xIndex][yIndex];
+            }
+        }
+    }
 }
