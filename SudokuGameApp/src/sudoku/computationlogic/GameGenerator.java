977a575a0a47c0f3616deefc4efcaa2a88af87ed
Hariprasad <hariprasadjnv963@gmail.com>
2025-10-12 22:33:33 +0530

    Add logic to check solvable

--- SudokuGameApp/src/sudoku/computationlogic/GameGenerator.java
+++ SudokuGameApp/src/sudoku/computationlogic/GameGenerator.java
@@ -15,6 +15,22 @@ public class GameGenerator {
     }
 
     private static int[][] unsolveGame(int[][] solvedGame) {
+        Random random = new Random(System.currentTimeMillis());
+
+        boolean solvable = false;
+        int[][] solvableArray = new int[GRID_BOUNDARY][GRID_BOUNDARY];
+
+        while (solvable == false) {
+            SudokuUtilities.copySudokuArrayValues(solvedGame, solvableArray);
+
+            int index = 0;
+
+            while (index < 40) {
+                int xCoordinate = random.nextInt(GRID_BOUNDARY);
+                int yCoordinate = random.nextInt(GRID_BOUNDARY);
+            }
+        }
+        return new int[0][];
     }
 
     private static int[][] getSolvedGame() {
