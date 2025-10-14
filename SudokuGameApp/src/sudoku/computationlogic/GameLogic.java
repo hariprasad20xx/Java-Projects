39145be9c696dce77467edc669de7e6d3b189b42
Hariprasad <hariprasadjnv963@gmail.com>
2025-10-14 15:10:28 +0530

    Create method getNewGame

--- SudokuGameApp/src/sudoku/computationlogic/GameLogic.java
+++ SudokuGameApp/src/sudoku/computationlogic/GameLogic.java
@@ -1,4 +1,15 @@
 package sudoku.computationlogic;
 
+import sudoku.constants.GameState;
+import sudoku.problemdomain.SudokuGame;
+
 public class GameLogic {
+
+    public static SudokuGame getNewGame() {
+        return new SudokuGame(
+                GameState.NEW,
+                GameGenerator.getNewGameGrid()
+        );
+    }
+
 }
