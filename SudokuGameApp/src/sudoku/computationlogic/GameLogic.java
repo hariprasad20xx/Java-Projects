7009f6c4f959e2afc9ba26730641ef709a371baa
Hariprasad <hariprasadjnv963@gmail.com>
2025-10-14 15:25:09 +0530

    Create method tilesAreNotFilled

--- SudokuGameApp/src/sudoku/computationlogic/GameLogic.java
+++ SudokuGameApp/src/sudoku/computationlogic/GameLogic.java
@@ -3,6 +3,8 @@ package sudoku.computationlogic;
 import sudoku.constants.GameState;
 import sudoku.problemdomain.SudokuGame;
 
+import static sudoku.problemdomain.SudokuGame.GRID_BOUNDARY;
+
 public class GameLogic {
 
     public static SudokuGame getNewGame() {
@@ -16,5 +18,15 @@ public class GameLogic {
         if (sudokuIsInvalid(grid)) return GameState.ACTIVE;
         if (tilesAreNotFilled(grid)) return GameState.ACTIVE;
         return GameState.COMPLETE;
+    }
+
+    private static boolean tilesAreNotFilled(int[][] grid) {
+        for (int xIndex = 0; xIndex < GRID_BOUNDARY; xIndex++) {
+            for (int yIndex = 0; yIndex < GRID_BOUNDARY; yIndex++) {
+                if (grid[xIndex][yIndex] == 0) return true;
+            }
+        }
+
+        return false;
     }
 }
