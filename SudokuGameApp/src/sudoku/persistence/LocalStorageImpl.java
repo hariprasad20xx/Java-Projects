695c5335cd429d28e060aa2b9c26768a36a822cd
Hariprasad <hariprasadjnv963@gmail.com>
2025-10-18 21:03:52 +0530

    Implement method updateGameData

--- SudokuGameApp/src/sudoku/persistence/LocalStorageImpl.java
+++ SudokuGameApp/src/sudoku/persistence/LocalStorageImpl.java
@@ -1,4 +1,33 @@
 package sudoku.persistence;
 
-public class LocalStorageImpl {
+import sudoku.problemdomain.IStorage;
+import sudoku.problemdomain.SudokuGame;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class LocalStorageImpl implements IStorage {
+    private static File GAME_DATA = new File(
+            System.getProperty("user.home"),
+            "gamedata.txt"
+    );
+
+    @Override
+    public void updateGameData(SudokuGame game) throws IOException {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(GAME_DATA);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(game);
+            objectOutputStream.close();
+        } catch (IOException e) {
+            throw new IOException("Unable to access Game Data");
+        }
+    }
+
+    @Override
+    public SudokuGame getGameData() throws IOException {
+        return null;
+    }
 }
