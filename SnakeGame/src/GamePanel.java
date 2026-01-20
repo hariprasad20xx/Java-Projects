f8a45fb41ef2e6d33e29f0be99ca72b875e6c890
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-20 20:30:22 +0530

    Implement ActionListener interface

--- SnakeGame/src/GamePanel.java
+++ SnakeGame/src/GamePanel.java
@@ -1,4 +1,11 @@
 import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
