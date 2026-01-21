d1db90fd4868d422bf5d59bda50649f0e819f9dd
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-21 16:54:28 +0530

    Create inner class MyKeyAdapter

--- SnakeGame/src/GamePanel.java
+++ SnakeGame/src/GamePanel.java
@@ -2,6 +2,7 @@ import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
 
 public class GamePanel extends JPanel implements ActionListener {
 
@@ -32,6 +33,10 @@ public class GamePanel extends JPanel implements ActionListener {
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
 
     }
 }
