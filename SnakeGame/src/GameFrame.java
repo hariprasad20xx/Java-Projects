a8e1584f4ff31ee32cd4609a5804633c6800ed64
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-21 16:35:35 +0530

    Setup game frame

--- SnakeGame/src/GameFrame.java
+++ SnakeGame/src/GameFrame.java
@@ -3,6 +3,12 @@ import javax.swing.JFrame;
 public class GameFrame extends JFrame {
 
     GameFrame() {
-
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
     }
 }
