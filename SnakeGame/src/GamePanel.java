ff42caecf884b56d02eafa6137b899a381049ea4
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-21 17:00:08 +0530

    Modify import statements

--- SnakeGame/src/GamePanel.java
+++ SnakeGame/src/GamePanel.java
@@ -1,9 +1,7 @@
-import javax.swing.JPanel;
-import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
 
