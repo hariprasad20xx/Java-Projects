b6133d3fa72e668a283d4153b3e4d73df9e7dbc7
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-22 20:22:27 +0530

    Initialize game panel components in constructor

--- SnakeGame/src/GamePanel.java
+++ SnakeGame/src/GamePanel.java
@@ -10,8 +10,8 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
     static final  int DELAY = 75;
-    final int x[] = new int[GAME_UNITS];
-    final int y[] = new int[GAME_UNITS];
+    final int[] x = new int[GAME_UNITS];
+    final int[] y = new int[GAME_UNITS];
     int bodyParts = 6;
     int appleEaten;
     int appleX;
@@ -22,7 +22,12 @@ public class GamePanel extends JPanel implements ActionListener {
     Random random;
 
     GamePanel() {
-
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
     }
 
     public void startGame() {
