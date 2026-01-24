98049bfe37047055db98e0c904e45d9352406e01
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-24 22:33:50 +0530

    Refactor code

--- SnakeGame/src/GamePanel.java
+++ SnakeGame/src/GamePanel.java
@@ -41,10 +41,7 @@ public class GamePanel extends JPanel implements ActionListener {
     }
     public void draw(Graphics g) {
         if (running) {
-            /*for (int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++) {
-                g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
-                g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
-            }*/
+
             g.setColor(Color.red);
             g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 
@@ -67,8 +64,8 @@ public class GamePanel extends JPanel implements ActionListener {
         }
     }
     public void newApple() {
-        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
-        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+        appleX = random.nextInt((SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        appleY = random.nextInt((SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
     }
     public void move() {
         for (int i = bodyParts; i > 0; i--) {
@@ -118,7 +115,7 @@ public class GamePanel extends JPanel implements ActionListener {
         if (y[0] < 0) {
             running = false;
         }
-        // check if head touches right border
+        // check if head touches bottom border
         if (y[0] > SCREEN_HEIGHT) {
             running = false;
         }
