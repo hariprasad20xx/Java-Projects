6bdd38575b1f138ccf619a44b02b9bebef15ab77
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-11 21:37:35 +0530

    Import required classes

--- Hangman/src/Hangman.java
+++ Hangman/src/Hangman.java
@@ -1,3 +1,11 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Hangman {
 
     public static void main(String[] args) {
