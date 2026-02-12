9fa80a3d4fbd5f2f35768bc5693fb438df195a23
Hariprasad <hariprasadjnv963@gmail.com>
2026-02-12 21:38:05 +0530

    Add words to ArrayList

--- Hangman/src/Hangman.java
+++ Hangman/src/Hangman.java
@@ -3,12 +3,14 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Hangman {
 
     public static void main(String[] args) {
-        ArrayList<String> words = new ArrayList<>();
+        ArrayList<String> words = new ArrayList<>(List.of("banana", "pineapple", "apple", "orange", "watermelon", "blueberry"));
+
     }
 }
